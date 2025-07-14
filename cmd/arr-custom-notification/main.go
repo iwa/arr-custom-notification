da546ebea65356d924f7c1fc4db1cedfd71ce16c
iwa <iwaongit@gmail.com>
2025-07-15 00:04:58 +0200

    feat: make main function work

--- cmd/arr-custom-notification/main.go
+++ cmd/arr-custom-notification/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "github.com/iwa/arr-custom-notification/internal/utils"
+import (
+	"fmt"
+	"os"
+
+	"github.com/iwa/arr-custom-notification/internal/utils"
+)
 
 func main() {
 	botToken, chatId := utils.ImportEnv()
+
+	sonarrEventType := os.Getenv("sonarr_eventtype")
+	radarrEventType := os.Getenv("radarr_eventtype")
+
+	if sonarrEventType == "" && radarrEventType == "" {
+		panic("[FATAL] No event type found.")
+	}
+
+	var message string
+
+	if sonarrEventType != "" {
+		fmt.Println("Sonarr - Event type:", sonarrEventType)
+		message = utils.SonarrMessage(sonarrEventType)
+	} else if radarrEventType != "" {
+		fmt.Println("Radarr - Event type:", radarrEventType)
+		message = utils.RadarrMessage(radarrEventType)
+	}
+
+	err := utils.SendTelegramMessage(chatId, botToken, message)
+	if err != nil {
+		fmt.Printf("Error sending Telegram message: %s\n", err)
+		panic("[FATAL] Failed to send Telegram message.")
+	}
 }
