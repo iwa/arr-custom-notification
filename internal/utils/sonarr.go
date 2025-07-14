d6e5996e57955d8f27d45a775fa4abb0768878bd
iwa <iwaongit@gmail.com>
2025-07-14 23:56:44 +0200

    feat: optimize & format outputs from SonarrMessage

--- internal/utils/sonarr.go
+++ internal/utils/sonarr.go
@@ -5,43 +5,45 @@ import (
 	"os"
 )
 
-func SonarrMessage(eventType string) (string, error) {
+func SonarrMessage(eventType string) string {
 	var message string
 
 	switch eventType {
 	case "Grab":
 		formattedEpisodes, err := formatEpisodes(os.Getenv("sonarr_release_seasonnumber"), os.Getenv("sonarr_release_episodenumbers"), os.Getenv("sonarr_release_episodetitles"))
 		if err != nil {
-			fmt.Println(err)
-			return "", err
+			fmt.Println(eventType, err)
+			formattedEpisodes = err.Error()
 		}
 		message = fmt.Sprintf("<b>🔵🔍 Sonarr - Episode Grabbed</b>\n%s\n%s", os.Getenv("sonarr_series_title"), formattedEpisodes)
 
 	case "Download":
 		formattedEpisodes, err := formatEpisodes(os.Getenv("sonarr_episodefile_seasonnumber"), os.Getenv("sonarr_episodefile_episodenumbers"), os.Getenv("sonarr_episodefile_episodetitles"))
 		if err != nil {
-			fmt.Println(err)
-			return "", err
+			fmt.Println(eventType, err)
+			formattedEpisodes = err.Error()
 		}
+
 		if os.Getenv("sonarr_isupgrade") == "True" {
 			message = fmt.Sprintf("<b>🔵🆙 Sonarr - Episode Upgraded</b>\n%s\n%s", os.Getenv("sonarr_series_title"), formattedEpisodes)
 		} else {
-			message = fmt.Sprintf("<b>🔵⬇️ Sonarr - Episode Downloaded</b>\n%s\n%s", os.Getenv("sonarr_series_title"), formattedEpisodes)
+			message = fmt.Sprintf("<b>🔵☑️ Sonarr - Episode Downloaded</b>\n%s\n%s", os.Getenv("sonarr_series_title"), formattedEpisodes)
 		}
 
 	case "Rename":
-		message = fmt.Sprintf("<b>🔵🛠️ Sonarr - Episode Renamed</b>\n%s\n`%s`", os.Getenv("sonarr_series_title"), os.Getenv("sonarr_series_path"))
+		message = fmt.Sprintf("<b>🔵🛠️ Sonarr - Episode Renamed</b>\n%s\n<code>%s</code>", os.Getenv("sonarr_series_title"), os.Getenv("sonarr_series_path"))
 
 	case "EpisodeFileDelete":
 		formattedEpisodes, err := formatEpisodes(os.Getenv("sonarr_episodefile_seasonnumber"), os.Getenv("sonarr_episodefile_episodenumbers"), os.Getenv("sonarr_episodefile_episodetitles"))
 		if err != nil {
-			fmt.Println(err)
-			return "", err
+			fmt.Println(eventType, err)
+			formattedEpisodes = err.Error()
 		}
+
 		message = fmt.Sprintf("<b>🔵🗑️ Sonarr - Episode Deleted</b>\n%s\n%s", os.Getenv("sonarr_series_title"), formattedEpisodes)
 
 	case "SeriesDelete":
-		message = fmt.Sprintf("<b>🔵🗑️ Sonarr - Serie Deleted</b>\n%s\n`%s`", os.Getenv("sonarr_series_title"), os.Getenv("sonarr_series_path"))
+		message = fmt.Sprintf("<b>🔵🗑️ Sonarr - Serie Deleted</b>\n%s\n<code>%s</code>", os.Getenv("sonarr_series_title"), os.Getenv("sonarr_series_path"))
 
 	case "HealthIssue":
 		switch os.Getenv("sonarr_health_issue_level") {
@@ -56,15 +58,15 @@ func SonarrMessage(eventType string) (string, error) {
 		}
 
 	case "ApplicationUpdate":
-		message = fmt.Sprintf("<b>🔵🔧 Sonarr - Update</b>\n`%s -> %s`\n%s", os.Getenv("sonarr_update_previousversion"), os.Getenv("sonarr_update_newversion"), os.Getenv("sonarr_update_message"))
+		message = fmt.Sprintf("<b>🔵🔧 Sonarr - Update</b>\n<code>%s -> %s</code>\n%s", os.Getenv("sonarr_update_previousversion"), os.Getenv("sonarr_update_newversion"), os.Getenv("sonarr_update_message"))
 
 	case "Test":
 		message = "<b>🔵🔧 Sonarr - Test</b>\nThis is a test notification."
 
 	default:
-		fmt.Printf("Unknown event type: %s\n", eventType)
-		return "", fmt.Errorf("unknown event type: %s", eventType)
+		fmt.Printf("[WARN] Sonarr - Unknown event type: %s\n", eventType)
+		message = fmt.Sprintf("<b>🔵 Sonarr - Error</b>\nEvent type: %s", eventType)
 	}
 
-	return message, nil
+	return message
 }
